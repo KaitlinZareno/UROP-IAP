a32afc1651bfd66a3ab392685e4dc1ddcf96ab1c
KaitlinZareno <kaitlinzareno@gmail.com>
2020-01-17 15:22:48 -0500

    working program that merges 3 csv files

--- csv_merge.py
+++ csv_merge.py
@@ -10,15 +10,19 @@ import pandas as pd
 import os
 import re
 
-
-names = []
-affect_label_data = []
-
-affect_labels = os.listdir("./p01_s1_al")
-
+#get csv files 
+def get_affect_labels(path, suffix = ".csv"):
+    filenames = os.listdir(path)
+    csv_files = []
+    for name in filenames:
+        if name.endswith(suffix):
+            csv_files.append(name)
+    return csv_files
 
 #generate data frames for each csv file in directory 
-def to_df():
+def to_df(affect_labels):
+    affect_label_data= [] 
+    names = []
     for label in affect_labels:   
         #get names of the annotators 
         r = re.findall('([A-Z][a-z]+)',label)
@@ -32,18 +36,26 @@ def to_df():
         #add all dataframes into an array -- each element is a dataset from a different annotator
         affect_label_data.append(dataFrame)
     
+    #print(affect_label_data[0])
     return affect_label_data
 
 def merge(affect_label_data):
-    #merge first two dataframes in an array 
-    new_df = pd.merge(affect_label_data[0], affect_label_data[1], how='left', left_on=['clip-name','index'], right_on = ['clip-name','index'])
-    
+    #merge takes in an array 
+    new_df = pd.merge(affect_label_data[0], affect_label_data[1],  how='left', left_on=['clip-name','index'], right_on = ['clip-name','index'])    
     return new_df
 
 
-if __name__ == '__main__':
-    #merge first two dataframes in list ()
-    data_array = to_df()
+if __name__ == '__main__':  
+    
+    #initialize clip_name and path
+    clip_name = "p01_s1"
+    path = "./p01_s1_al"
+    
+    #get csv files, transform to dataFrames and rename columns to include annotator's name
+    affect_labels = get_affect_labels(path)
+    data_array = to_df(affect_labels)
+    
+    #merge data of first two annotators
     merge1_and2 = merge(data_array)
     
     #create new list of dataFrames that contains the merged dataFrames and the last dataFrame in directory
@@ -53,4 +65,6 @@ if __name__ == '__main__':
     
     #merge all dataFrames
     merge_all = merge(new_data_array)
-    #merge_all.to_csv('./p01_s1_al/mergedAffecr')
+    #save merged dataFrames as csv
+    merge_all.to_csv('./merged_affect_labels/'+clip_name+".csv")
+    
