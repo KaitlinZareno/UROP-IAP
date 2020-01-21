a8580005ae1c458f8f82414e146d3a1368ca8742
KaitlinZareno <kaitlinzareno@gmail.com>
2020-01-21 14:52:26 -0500

    working code. copy's to_df() and get_affect_labels() functions takes a list, paths, as a parameter

--- csv_merge.py
+++ csv_merge.py
@@ -10,6 +10,16 @@ import pandas as pd
 import os
 import re
 
+#get paths of subdirectories that start with letter p (indicates video clips) . ex) p01_s1
+def get_paths():
+    path_list = []
+    name_list = []
+    for name in os.listdir("."):
+        if name.startswith("p"):
+            path_list.append(os.path.abspath(name))
+            name_list.append(name)
+    return path_list,name_list
+
 #get csv files 
 def get_affect_labels(path, suffix = ".csv"):
     filenames = os.listdir(path)
@@ -20,7 +30,7 @@ def get_affect_labels(path, suffix = ".csv"):
     return csv_files
 
 #generate data frames for each csv file in directory 
-def to_df(affect_labels):
+def to_df(affect_labels,path):
     affect_label_data= [] 
     names = []
     for label in affect_labels:   
@@ -28,15 +38,14 @@ def to_df(affect_labels):
         r = re.findall('([A-Z][a-z]+)',label)
         names.append(r[0])
         #convert csv into data frame
-        dataFrame = pd.read_csv("./p01_s1_al/"+label)
+        dataFrame = pd.read_csv(path+'/'+label)
         #rename columns on affect and valence (parent and child) to include annotator's name
         dataFrame = dataFrame.rename(columns={"answer-timestamp" : "answer-timestamp ("+ r[0]+")", "Storyreading" : "Storyreading ("+ r[0]+")",
                                               "PARENT's valence": "PARENT's valence ("+ r[0]+")", "CHILD's valence": "CHILD's valence ("+ r[0]+")", 
                                               "PARENT's arousal" : "PARENT's arousal ("+ r[0]+")", "CHILD's arousal" : "CHILD's arousal ("+ r[0]+")"})
         #add all dataframes into an array -- each element is a dataset from a different annotator
         affect_label_data.append(dataFrame)
-    
-    #print(affect_label_data[0])
+        
     return affect_label_data
 
 def merge(affect_label_data):
@@ -47,24 +56,26 @@ def merge(affect_label_data):
 
 if __name__ == '__main__':  
     
-    #initialize clip_name and path
-    clip_name = "p01_s1"
-    path = "./p01_s1_al"
+    #initialize paths of subdirectories containing clip annotations
+    paths,names = get_paths()
+    #print(paths,names)
     
-    #get csv files, transform to dataFrames and rename columns to include annotator's name
-    affect_labels = get_affect_labels(path)
-    data_array = to_df(affect_labels)
-    
-    #merge data of first two annotators
-    merge1_and2 = merge(data_array)
-    
-    #create new list of dataFrames that contains the merged dataFrames and the last dataFrame in directory
-    new_data_array = []
-    new_data_array.append(merge1_and2)
-    new_data_array.append(data_array[2])
-    
-    #merge all dataFrames
-    merge_all = merge(new_data_array)
-    #save merged dataFrames as csv
-    merge_all.to_csv('./merged_affect_labels/'+clip_name+".csv")
-    
+    for x in range(len(paths)):
+        #get csv files, transform to dataFrames and rename columns to include annotator's name
+        affect_labels = get_affect_labels(paths[x])
+        data_array = to_df(affect_labels,paths[x])
+        print(data_array)
+        
+        #merge data of first two annotators
+        merge1_and2 = merge(data_array)
+        
+        #create new list of dataFrames that contains the merged dataFrames and the last dataFrame in directory
+        new_data_array = []
+        new_data_array.append(merge1_and2)
+        new_data_array.append(data_array[2])
+        
+        #merge all dataFrames
+        merge_all = merge(new_data_array)
+        #print(merge_all)
+        #save merged dataFrames as csv
+        #merge_all.to_csv('./merged_affect_labels/'+names[x]+'.csv')
